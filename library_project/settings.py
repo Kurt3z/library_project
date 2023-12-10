db97fbe79ed8b4536615f19f83352a100be0c04f
Tiago Pinto <tiago.webdev10@gmail.com>
2023-12-10 17:22:04 +0000

    Add MySQL Integration

--- library_project/settings.py
+++ library_project/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "library_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'PASSWORD': 'pacosn10',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
 
