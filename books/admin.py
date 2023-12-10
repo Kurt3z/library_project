020ae8540d7c8e8a9465175e394aff7af72e476d
Tiago Pinto <tiago.webdev10@gmail.com>
2023-12-10 21:10:45 +0000

    Add slug field to book model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
