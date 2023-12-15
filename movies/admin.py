24b598fd9945bd04dada75d6735bf9240871bf01
Tiago Pinto <tiago.webdev10@gmail.com>
2023-12-15 22:25:02 +0000

    Register Movies Models in Admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Director)
